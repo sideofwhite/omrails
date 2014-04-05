542d38d5b2c51269f26a6a8a9e8ee4bd8f09a990
Nevan Whiteside <nevanwhiteside@gmail.com>
2014-04-04 22:16:37 -0400

    tweaks

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,12 @@
 class UsersController < ApplicationController
   def show
   @user = User.friendly.find(params[:id])
-  @comments = @user.get_voted Comment
+  @questions = @user.get_voted Question
   
   end
 
   def questions
   @user = User.friendly.find(params[:id])
-  @posts = @user.posts
   @comments = @user.comments.page(params[:page]).per_page(25)
-  @questions = @user.questions
   end 
 end
