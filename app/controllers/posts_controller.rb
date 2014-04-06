d045ac34bc42ec2e6824a6551faef7cc8dff52e9
Nevan Whiteside <nevanwhiteside@gmail.com>
2014-04-06 15:18:32 -0400

    tweaks

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,6 +22,7 @@ if params[:tag]
 @posts = Post.tagged_with(params[:tag]).page(params[:page]).per_page(25)
 else
 @posts = Post.order('cached_votes_total desc').page(params[:page]).per_page(25).limit(25).offset(1)
+@postsmobile = Post.order('cached_votes_total desc').page(params[:page]).per_page(25)
 @toppost = Post.order('cached_votes_total desc').limit(1)
 end
 
