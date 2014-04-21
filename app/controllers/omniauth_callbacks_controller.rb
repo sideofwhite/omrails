8c62061542399adb164824144a5fbb53b9463675
Nevan Whiteside <nevanwhiteside@gmail.com>
2014-04-20 20:01:55 -0400

    tweaks

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,11 +1,8 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 def all
     user = User.from_omniauth(request.env["omniauth.auth"])
-    if user.persisted? and user.email.nil?
-     sign_in(user) 
-     redirect_to '/info'
-    elsif user.persisted? and user.email.present?
-     sign_in_and_redirect user
+    if user.persisted?
+      sign_in_and_redirect user
     else
       session["devise.user_attributes"] = user.attributes
       redirect_to new_user_registration_url
