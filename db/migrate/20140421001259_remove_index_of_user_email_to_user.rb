bfd5b5c7c26187eb3ff60affa0f6f35521a469a3
Nevan Whiteside <nevanwhiteside@gmail.com>
2014-04-20 20:13:45 -0400

    tweaks

---
+++ db/migrate/20140421001259_remove_index_of_user_email_to_user.rb
@@ -0,0 +1,8 @@
+class RemoveIndexOfUserEmailToUser < ActiveRecord::Migration
+  def up
+      remove_index :users, :email
+  end
+  def down
+      add_index :users, :email,:unique => true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140330211818) do
+ActiveRecord::Schema.define(version: 20140421001259) do
 
   create_table "answers", force: true do |t|
     t.text     "body"
@@ -171,7 +171,6 @@ ActiveRecord::Schema.define(version: 20140330211818) do
     t.string   "slug"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   add_index "users", ["slug"], name: "index_users_on_slug"
 
